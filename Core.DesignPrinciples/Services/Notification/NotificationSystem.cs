dbaf5628a6b87fbb64e55a98b78190502bd60143
Saurabh Kumar <saurabhk2323@gmail.com>
2025-06-16 02:21:14 +0530

    If I have an interface IXYZ, implemented by a base class XYZ, and child classes XYZ1 and XYZ2 override a virtual method ABC(), how can the DI (through IXYZ) know which child's override to use, since both override the same method?

--- Core.DesignPrinciples/Services/Notification/NotificationSystem.cs
+++ Core.DesignPrinciples/Services/Notification/NotificationSystem.cs
@@ -2,6 +2,13 @@
 
 namespace Core.DesignPrinciples.Services.Notification
 {
+    /*NotificationSystem has implemented INotificationSystem
+     There is a function - SendNotification
+    - The Notification System is acting as a base class
+    - EmailNotification, SMSNotification, PushNotification - Children
+    - If we are using INotificationSystem as DI inside a class, then from there how to call a specific children function
+    - 
+     */
     public class NotificationSystem: INotificationSystem
     {
         private readonly ILogger<NotificationSystem> _logger;
