627cf07bbed7995196a9ef8918ada5a037f2d65b
Saurabh Kumar <saurabhk2323@gmail.com>
2025-06-24 17:05:34 +0530

    Design Pattern: Command - refactored

---
+++ Core.DesignPatterns.Behavioral/Command/ICommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.DesignPatterns.Behavioral.Command
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+}
---
+++ Core.DesignPatterns.Behavioral/Command/OrderCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.DesignPatterns.Behavioral.Command
+{
+    public class OrderCommand : ICommand
+    {
+        private readonly string _orderId;
+        private readonly string _productId;
+
+        public OrderCommand(string orderId, string productId)
+        {
+            _orderId = orderId;
+            _productId = productId;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine($"[OrderService] Order {_orderId} placed for {_productId}.");
+        }
+    }
+}
